d8032a23c26ed177345db1f27f3b82285129918a
Ash-2k3 <ash.me.cse@gmail.com>
2023-05-15 14:13:21 +0530

    Conda dependencies and library commands partial

---
+++ ml_setup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Install Notebook using the following command
+conda install -c anaconda notebook
+
+# Numpy: is a library for the Python programming language, adding support for la# rge, multi-dimensional arrays and matrices
+conda install -c anaconda numpy
+
+# Matplotlib: a comprehensive library for creating static, animated, and interac# tive visualizations
+conda install -c conda-forge matplotlib
+
+# Seaborn: data visualization library based on matplotlib
+conda install -c anaconda seaborn
+
+# Pandas: fast, powerful, flexible and easy to use open-source data analysis and# manipulation tool.
+conda install -c anaconda pandas
+
+
