d68c99de1971211535d861623c35f21eca160e21
Ash-2k3 <ash.me.cse@gmail.com>
2023-05-15 15:08:53 +0530

    Changed from conda to pip

--- ml_setup.sh
+++ ml_setup.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
-# Install Notebook using the following command
-conda install -c anaconda notebook
-
 # Numpy: is a library for the Python programming language, adding support for la# rge, multi-dimensional arrays and matrices
-conda install -c anaconda numpy
+pip install numpy
 
 # Matplotlib: a comprehensive library for creating static, animated, and interac# tive visualizations
-conda install -c conda-forge matplotlib
+pip install matplotlib
 
 # Seaborn: data visualization library based on matplotlib
-conda install -c anaconda seaborn
+pip install seaborn
 
-# Pandas: fast, powerful, flexible and easy to use open-source data analysis and# manipulation tool.
-conda install -c anaconda pandas
+# Pandas: fast, powerful, flexible and easy to use open-source data analysis and# manipulation too
+pip install pandas
 
+# Keras: To be Added
+pip install keras
+
+# TensorFlow: To be Added
+pip install tensorflow
+
+# scikit-learn: To be Added
+pip install scikit-learn
+
+# OpenCV: To be Added
+pip install opencv-python
+
+# Launch jupyter notebook
+jupyter notebook
 
